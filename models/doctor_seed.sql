09fe18efd611102d5ab8df3e116622f005ec3229
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-11-02 13:39:06 -0400

    added sign up form to handlebars files

---
+++ models/doctor_seed.sql
@@ -0,0 +1,4 @@
+USE Appointa;
+
+INSERT INTO Doctors (firstname, lastname, discipline, email, phone, username, password, account_id, createdAt, updatedAt)
+VALUES ('Eric','Vargas','cardiologist','eric.vargas719@gmail.com','929-600-4784', 'eric.vargas719@gmail.com','eric','12345','2019-10-01 11:03:15','2019-10-01 11:03:15')
