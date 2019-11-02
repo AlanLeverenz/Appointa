761cff9907b176f50d832ce1a0e9d20196d39efe
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-11-01 22:14:07 -0400

    added the routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,20 +3,53 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+    res.render("index", {});
+  });
+
+  //Load sign up page
+  app.get("/signup", function(req, res) {
+    res.render("signup", {
+    });
+  });
+
+  //Load sign in page
+  app.get("/signin", function(req, res) {
+    res.render("signin", {
+    });
+  });
+
+  // Load dashboard page by id for that user/doctor
+  app.get("/:id/dashboard", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("dashboard", {
+        example: dbExample
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  //Load profile page
+  app.get("/:id/profile", function(req, res) {
+    res.render("profile", {
+    });
+  });
+
+  //Load appointment page
+  app.get("/:id/appointment", function(req, res) {
+    res.render("appointment", {
+    });
+  });
+
+  //Load create appointment page
+  app.get("/:id/createappointment", function(req, res) {
+    res.render("createappointment", {
+    });
+  });
+
+  //Load message page
+  app.get("/:id/message", function(req, res) {
+    res.render("message", {
     });
   });
 
